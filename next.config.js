9bb6537e9ef9a95cd46734419cb46bda6211234b
moro <moro.remy879@gmail.com>
2024-01-20 21:46:55 +0100

    Installation of the first SASS syles

--- next.config.js
+++ next.config.js
@@ -6,6 +6,16 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "src/styles")],
   },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        pathname: "/t/p/w500/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Header from "@/components/Header/Header";
 import "./globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { roboto,montserrat } from "@/fonts";
 
 export const metadata = {
   title: "Create Next App",
@@ -10,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${roboto.className} ${montserrat.className}`}>
         <Header />
         <main>{children}</main>
       </body>
