dad57ed66214b0faadc3d010dd5cb2becaccb071
moro <moro.remy879@gmail.com>
2024-01-20 21:17:07 +0100

    init  project movie

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
-import { Inter } from "next/font/google";
+import Header from "@/components/Header/Header";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Header />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
